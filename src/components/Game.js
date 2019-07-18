9b9a56a57f8d0f6980d9a1660a37b371c2da3ff3
Wolfenek <wolfenek@gmail.com>
2019-07-18 14:24:58 +0200

    base functionality working

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TileList from "./TileList";
 import sound1 from "../sounds/1.mp3";
 import sound2 from "../sounds/2.mp3";
@@ -7,41 +7,41 @@ import sound4 from "../sounds/4.mp3";
 
 const dopeSounds = [
   {
-    id: 1,
+    id: "cat1",
     dopeSound: sound1
   },
   {
-    id: 2,
+    id: "cat2",
     dopeSound: sound2
   },
   {
-    id: 3,
+    id: "cat3",
     dopeSound: sound3
   },
   {
-    id: 4,
+    id: "cat4",
     dopeSound: sound4
   }
 ];
 
 const ourTiles = [
   {
-    id: 1,
+    id: "cat1",
     picture: "http://lorempixel.com/150/150/cats/",
     alt: "pic1"
   },
   {
-    id: 2,
+    id: "cat2",
     picture: "http://lorempixel.com/150/150/cats/",
     alt: "pic2"
   },
   {
-    id: 3,
+    id: "cat3",
     picture: "http://lorempixel.com/150/150/cats/",
     alt: "pic3"
   },
   {
-    id: 4,
+    id: "cat4",
     picture: "http://lorempixel.com/150/150/cats/",
     alt: "pic4"
   }
@@ -49,32 +49,55 @@ const ourTiles = [
 
 const Game = () => {
   // Game started
-  const [playing, setPlaying] = React.useState(0);
-  // const togglePlay = () => setPlaying(!playing);
+  const [playing, setPlaying] = useState(0);
+  const [soundId, setSoundId] = useState(dopeSounds[playing].id);
+  // Get id of clicked tile
+  const [tileId, setTileId] = useState("");
+  const [score, setScore] = useState(0);
+  const [clickedNumber, setClickedNumber] = useState(0);
 
-  // React.useEffect(() => {
-  //   console.log("yeah played");
-  //   playing ? audio.play() : audio.pause();
-  // }, [playing]);
+  useEffect(() => {
+    
+      setPlaying((playing + 1) % dopeSounds.length);
+      console.log(dopeSounds[playing].id);
+      setSoundId(dopeSounds[playing].id)
+      audio.play();
+      if (tileId === soundId) {
+        console.log("success");
+        setScore(score + 1);
+      } else {
+        console.log("nope");
+      }
+  }, [clickedNumber])
 
-  function newSound() {
-    setPlaying((playing + 1) % dopeSounds.length);
-    console.log(playing);
-    audio.play();
-  }
 
   const sound = dopeSounds[playing].dopeSound;
   const audio = new Audio(sound);
+
+  const getTileId = id => {
+    setTileId(id);
+    setClickedNumber(clickedNumber +1);
+  };
+
   return (
     <>
       <button
-        onClick={newSound}
         className="btn btn-play"
         aria-label="start-button"
       >
         Play
       </button>
-      <TileList ourTiles={ourTiles} />
+      <TileList ourTiles={ourTiles} getTileId={getTileId} />
+      <br />
+      <br />
+      <br />
+      sound: {soundId}
+      <br />
+      tile: {tileId}
+      <div>You got {score} right!</div>
+      <br />
+      <br />
+      {clickedNumber}
     </>
   );
 };
--- src/components/TileList.js
+++ src/components/TileList.js
@@ -1,21 +1,20 @@
 import React from "react";
 
-const Tile = ({ tile: { picture, alt, id } }) => {
+const Tile = ({ tile: { picture, alt, id }, getTileId }) => {
   return (
-    <div className="tile">{id}
+    <div className="tile" onClick={() => getTileId(id)}>{id}
       <img src={picture} alt={alt} />
-      
     </div>
   );
 };
 
-const TileList = ({ ourTiles }) => {
+const TileList = ({ ourTiles, getTileId }) => {
   return (
-    <>
+    <div className="flex-container">
       {ourTiles.map(tile => {
-        return <Tile key={tile.id} tile={tile} />;
+        return <Tile key={tile.id} tile={tile} getTileId={getTileId} />;
       })}
-    </>
+    </div>
   );
 };
 
